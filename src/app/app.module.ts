60da29d64e3593809271882781d94021f4bda83c
manuel.bit <emmanuel_njuguna@outlook.com>
2019-10-28 10:51:41 +0300

    add the details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteComponent } from './quote/quote.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
-    QuoteComponent
+    QuoteComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
